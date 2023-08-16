1a50b192ae5fa4538a7175daa7cb90f2722ef570
Thomas Rayden <twrayden@gmail.com>
2023-08-16 15:47:50 +0800

    Convert to named functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+export type VisitMap<T extends object> = {
+  [K in keyof T]-?: (value: T[K]) => void;
+};
+
 /**
  * @name visit
  * @summary Run a function for each key of target object
@@ -9,16 +13,17 @@
  * @param map - strict object map with a function per key
  * @returns void
  */
-export type VisitMap<T extends object> = {
-  [K in keyof T]-?: (value: T[K]) => void;
-};
-
-export const visit = <T extends object>(obj: T, map: VisitMap<T>): void =>
-  Object.keys(obj).forEach((key) => {
+export function visit<T extends object>(obj: T, map: VisitMap<T>): void {
+  return Object.keys(obj).forEach((key) => {
     const _key = key as keyof T;
     if (!Object.hasOwnProperty.call(map, _key)) return;
     map[_key](obj[_key]);
   });
+}
+
+export type Trimmed<T extends object> = {
+  [P in keyof T as T[P] extends undefined ? never : P]: T[P];
+};
 
 /**
  * @name trim
@@ -30,11 +35,7 @@ export const visit = <T extends object>(obj: T, map: VisitMap<T>): void =>
  * @param obj - the target object
  * @returns new object with empty keys omitted
  */
-export type Trimmed<T extends object> = {
-  [P in keyof T as T[P] extends undefined ? never : P]: T[P];
-};
-
-export const trim = <T extends object>(obj: T): Trimmed<T> => {
+export function trim<T extends object>(obj: T): Trimmed<T> {
   const result = { ...obj };
   for (var prop in result) {
     if (
@@ -45,7 +46,7 @@ export const trim = <T extends object>(obj: T): Trimmed<T> => {
     }
   }
   return result as Trimmed<T>;
-};
+}
 
 /**
  * @name isEmpty
@@ -57,8 +58,9 @@ export const trim = <T extends object>(obj: T): Trimmed<T> => {
  * @param obj - the target object
  * @returns target object is empty
  */
-export const isEmpty = <T extends object>(obj: T) =>
-  Object.keys(obj).length === 0;
+export function isEmpty<T extends object>(obj: T) {
+  return Object.keys(obj).length === 0;
+}
 
 /**
  * @name omitKey
@@ -71,13 +73,13 @@ export const isEmpty = <T extends object>(obj: T) =>
  * @param key - the key to remove from target object
  * @returns new object with key omitted
  */
-export const omitKey = <T extends object>(
+export function omitKey<T extends object>(
   obj: T,
   key: keyof T
-): Omit<T, keyof T> => {
+): Omit<T, keyof T> {
   const { [key]: _, ...result } = obj;
   return result;
-};
+}
 
 /**
  * @name hasKeys
@@ -90,5 +92,8 @@ export const omitKey = <T extends object>(
  * @param keys - the keys to check for existence in target object
  * @returns target object contains specified keys
  */
-export const hasKeys = <T extends object>(obj: T, keys: string[]): boolean =>
-  keys.map((key) => Object.keys(obj).indexOf(key) !== -1).indexOf(true) !== -1;
+export function hasKeys<T extends object>(obj: T, keys: string[]): boolean {
+  return (
+    keys.map((key) => Object.keys(obj).indexOf(key) !== -1).indexOf(true) !== -1
+  );
+}
